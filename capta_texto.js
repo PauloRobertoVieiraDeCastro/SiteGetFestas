5e4eccd667e930a0f32a3c14b0cf954ae66e03bd
Paulo Roberto Vieira de Castro <44949683+PauloRobertoVieiraDeCastro@users.noreply.github.com>
2021-01-22 17:07:45 -0300

    Add files via upload

---
+++ capta_texto.js
@@ -0,0 +1,7 @@
+function save(){
+	content = document.getElementById('email').value; //pega o conteudo do arquivo
+	
+	document.querySelector(".msgemail").innerHTML = "Obrigado por se inscrever! A partir de agora você receberá nossas novidades no seu e-mail."
+	
+	
+}
